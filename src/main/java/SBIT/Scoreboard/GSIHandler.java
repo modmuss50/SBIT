a774c865e97d45aff6940121537dac384a8ccbeb
Joshua Fillingham <j.j.fillingham@edu.salford.ac.uk>
2019-03-05 13:02:31 +0000

    feat: start GSI handleing

--- src/main/java/SBIT/Scoreboard/GSIHandler.java
+++ src/main/java/SBIT/Scoreboard/GSIHandler.java
@@ -1,6 +1,21 @@
 package SBIT.Scoreboard;
 
 
-public class GSIHandler {
+import java.util.Timer;
+import java.util.TimerTask;
 
+
+public class GSIHandler {
+    public static void setup() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                ticker();
+            }
+        }, 0, 1000);
+    }
+    public static void ticker() {
+
+    }
 }
